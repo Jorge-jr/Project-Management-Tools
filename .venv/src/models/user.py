435ce0929ea1a66baad6f9538b3e9b0eba11a615
Jorge-jr <jsribeirojr@hotmail.com>
2024-02-03 22:43:09 -0300

    adding .venv

---
+++ .venv/src/models/user.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
